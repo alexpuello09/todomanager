54cb1351d74d5295bdbaa45524b3cda2fa320a8c
Alex Puello <alexpuello788@gmail.com>
2023-09-16 21:25:01 -1200

    Add /todos endpoint with POST request to create new todo

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask 
+from flask import Flask, request
 
-todo_list = [{"title": "Create git repository"
-
+todo_list = [{
+    "title": "Create git repository"
 }
-
 ]
 app = Flask(__name__)
 
 @app.get("/todos")
 def get_todos():
     return todo_list,200
+
+
+@app.post("/todos")
+def create_todo():
+    data = request.get_json()
+    new_todo = {"title": data["title"]}
+    todo_list.append(new_todo)
+    return new_todo, 201
     
